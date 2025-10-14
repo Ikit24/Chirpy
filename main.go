41d16774b436192b455a2ca253390f4eedac4f19
Ikit24 <szszattila2@gmail.com>
2025-10-14 20:10:53 +0300

    add(feat): middleware that increment fileserverHits

--- main.go
+++ main.go
@@ -3,24 +3,48 @@ package main
 import (
 	"log"
 	"net/http"
+	"fmt"
+	"sync/atomic"
 )
 
 type apiHandler struct{}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func (apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
 
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hits := cfg.fileserverHits.Load()
+	w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	apiCfg := &apiConfig{}
 	mux := http.NewServeMux()
 	server := http.Server{
 		Addr: ":8080",
 		Handler: mux,
 	}
 	
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
 	mux.Handle("/healthz", apiHandler{})
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServerHandler))
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
 	
 	log.Fatal(server.ListenAndServe())
 }
