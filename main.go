ea8f5913a525065ceae98ef04e2a4e164b394af6
Attila <szszattila2@gmail.com>
2025-10-19 12:17:41 +0300

    add: new endpoint to validate Chirp length

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-	"fmt"
 	"sync/atomic"
 )
 
@@ -24,12 +25,12 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	hits := cfg.fileserverHits.Load()
 	w.Write([]byte(fmt.Sprintf(`
-<html>
-  <body>
-    <h1>Welcome, Chirpy Admin</h1>
-    <p>Chirpy has been visited %d times!</p>
-  </body>
-</html>`, hits)))
+	<html>
+	  <body>
+	    <h1>Welcome, Chirpy Admin</h1>
+	    <p>Chirpy has been visited %d times!</p>
+	  </body>
+	</html>`, hits)))
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
@@ -45,19 +46,55 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	})
 }
 
+type parameters struct {
+	Body string `json:"body"`
+}
+
+type errResp struct {
+	Error string `json:"error"`
+}
+
+type okResp struct {
+	Valid bool `json:"valid"`
+}
+
+func (cfg *apiConfig) handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var params parameters
+	if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
+		resp, _ := json.Marshal(errResp{Error: "Something went wrong"})
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(resp)
+		return
+	}
+
+	if len(params.Body) > 140 {
+		resp, _ := json.Marshal(errResp{Error: "Chirp is too long"})
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(resp)
+		return
+	}
+
+	resp, _ := json.Marshal(okResp{Valid: true})
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
 func main() {
 	apiCfg := &apiConfig{}
 	mux := http.NewServeMux()
 	server := http.Server{
-		Addr: ":8080",
+		Addr:    ":8080",
 		Handler: mux,
 	}
-	
+
 	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
 	mux.Handle("GET /api/healthz", apiHandler{})
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServerHandler))
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
-	
+	mux.HandleFunc("POST /api/validate_chirp", apiCfg.handlerValidateChirp)
+
 	log.Fatal(server.ListenAndServe())
 }
