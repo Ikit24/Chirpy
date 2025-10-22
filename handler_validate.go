f5db9750f167502d1fee453f6077700b2a2a9225
Attila <szszattila2@gmail.com>
2025-10-22 19:40:58 +0300

    add: profane filtering for kerfuffle, sharbert, fornax

--- handler_validate.go
+++ handler_validate.go
@@ -3,34 +3,48 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
 
 type parameters struct {
 	Body string `json:"body"`
 }
 
-type errResp struct {
-	Error string `json:"error"`
-}
-
-type okResp struct {
-	Valid bool `json:"valid"`
-}
-
 func (cfg *apiConfig) handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 	var params parameters
 	if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
-		writeJSONError(w, http.StatusBadRequest, "Something went wrong")
+		respondWithError(w, http.StatusBadRequest, "Something went wrong")
 		return
 	}
 
 	if len(params.Body) > 140 {
-		writeJSONError(w, http.StatusBadRequest, "Chirp is too long")
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
 		return
 	}
-	writeJSON(w, http.StatusOK, okResp{Valid: true})
+
+	cleaned := cleanProfanity(params.Body)
+	respondWithJSON(w, http.StatusOK, map[string]string{"cleaned_body": cleaned})
 }
 
-func respondWithError(w http.RespondseWriter, code int, msg string) {}
+func cleanProfanity(body string) string {
+	banned := map[string]bool{"kerfuffle": true, "sharbert": true, "fornax": true}
+	tokens := strings.Split(body, " ")
+	for i, tok := range tokens {
+		if banned[strings.ToLower(tok)] {
+			tokens[i] = "****"
+		}
+	}
+	return strings.Join(tokens, " ")
+}
 
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {}
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	_ = json.NewEncoder(w).Encode(map[string]string{"error": msg})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	_ = json.NewEncoder(w).Encode(payload)
+}
