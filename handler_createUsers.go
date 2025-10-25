254de113fd01d5ca052f027fda8630e555fd0858
Attila <szszattila2@gmail.com>
2025-10-25 12:10:00 +0300

    small refactor for DRY

--- handler_createUsers.go
+++ handler_createUsers.go
@@ -10,16 +10,10 @@ type userParams struct {
 	Email string `json:"email"`
 }
 
-func respondError(w http.ResponseWriter, code int, msg string) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	_ = json.NewEncoder(w).Encode(map[string]string{"error": msg})
-}
-
 func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request) {
 	var params userParams
 	if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
-		respondError(w, http.StatusBadRequest, "invalid JSON")
+		respondWithError(w, http.StatusBadRequest, "invalid JSON")
 		return
 	}
 
@@ -40,4 +34,3 @@ func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request)
 	w.WriteHeader(http.StatusCreated)
 	_ = json.NewEncoder(w).Encode(out)
 }
-
