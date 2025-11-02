0061a4f08a86eccc117ad8ecd50432099989a4c3
Attila <szszattila2@gmail.com>
2025-11-02 13:07:40 +0200

    feat(add): create and validate JWT

---
+++ internal/auth/validateJWT.go
@@ -0,0 +1,34 @@
+package auth
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := &jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		claims,
+		func(t *jwt.Token) (any, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method")
+		}
+		return []byte(tokenSecret), nil
+		},
+	)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	if !token.Valid {
+		return uuid.UUID{}, fmt.Errorf("invalid token")
+	}
+
+	id, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	return id, nil
+}
