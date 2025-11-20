70ce8023a29e4e7d704dc4bdcfd9bc6217aa9982
Attila <szszattila2@gmail.com>
2025-11-20 19:56:31 +0200

    add: sql update for is_chirpy_red

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -7,10 +7,10 @@ VALUES (
 	$1,
 	$2
 )
-RETURNING id, created_at, updated_at, email;
+RETURNING id, created_at, updated_at, email, is_chirpy_red;
 
 -- name: GetUserByEmail :one
-SELECT id, created_at, updated_at, email, hashed_password
+SELECT id, created_at, updated_at, email, hashed_password, is_chirpy_red
 FROM users
 WHERE email = $1;
 
@@ -21,4 +21,12 @@ SET
 	hashed_password = $3,
 	updated_at = NOW()
 WHERE id = $1
-RETURNING id, created_at, updated_at, email;
+RETURNING id, created_at, updated_at, email, is_chirpy_red;
+
+-- name: UpgradeUserToChirpyRed :one
+UPDATE users
+SET
+	is_chirpy_red = TRUE,
+	updated_at = NOW()
+WHERE id = $1
+RETURNING id, created_at, updated_at, email, is_chirpy_red;
